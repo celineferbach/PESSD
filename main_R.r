f3d0f77c35dfd3fbd1599a9cc68952dc316f5179
celineferbach <celine.ferbach@gmail.com>
2026-03-06 15:30:01 +0000

    nettoyage main et ajout main_R

---
+++ main_R.r
@@ -0,0 +1,8 @@
+install.packages("languageserver")
+install.packages("httpgd")
+install.packages("radian")
+
+#chargement des données
+panel <- read.csv("panelV2.csv")
+head(panel)   # voir les premières lignes
+
